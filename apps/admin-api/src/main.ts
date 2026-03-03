460cd4641cf544e45d647c22cd46611de830a94c
Godson <gnqnpn@gmail.com>
2026-03-03 17:49:36 +0100

    some changes

--- apps/admin-api/src/main.ts
+++ apps/admin-api/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
 
   app.enableCors({
-    origin: ['https://celebratedboy.netlify.app', 'http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://famoadmin.netlify.app', 'http://localhost:5173', 'http://localhost:5174'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
